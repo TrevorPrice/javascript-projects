226e34e39410ff5dd278e742f2a3171276159c4b
TrevorPrice <tprice1245@gmail.com>
2024-01-22 20:59:40 -0600

    Completed Loops Studio

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,13 @@
 // create an array variable containing the names
 
+let friends = ["Sage", "Carl", "Reed", "Drew"];
+let family = ["Mom", "Dad", "Lauren", "Devin", "Caitlyn", "Kendall", "Morgan"];
+
 // write a for loop that prints each name on a different line
+
+for (let i = 0; i < friends.length; i++) {
+    console.log(friends[i]);
+}
+for (let i = 0; i <family.length; i++) {
+    console.log(family[i]);
+}
