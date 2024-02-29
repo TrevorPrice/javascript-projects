14df75f4434fce4c0a7feea0c20fd4510d8cfbb6
TrevorPrice <tprice1245@gmail.com>
2024-02-29 15:51:35 -0600

    forms exercise complete

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,1 +1,9 @@
 //Code Your Solution Below
+function init() {
+    const runSimulation = document.getElementById("run");
+
+    runSimulation.addEventListener("click", (event) => {
+        window.confirm("Confirm that you want to run the simulation.")
+    });
+
+}
