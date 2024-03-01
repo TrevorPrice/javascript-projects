df8369ce9601e02ef7eb945ebca401f8075c7716
TrevorPrice <tprice1245@gmail.com>
2024-02-29 20:46:42 -0600

    Forms studio completed

--- user-input-with-forms/exercises/script.js
+++ user-input-with-forms/exercises/script.js
@@ -1,9 +1,30 @@
 //Code Your Solution Below
-function init() {
-    const runSimulation = document.getElementById("run");
+// function init() {
+//     const runSimulation = document.getElementById("run");
 
-    runSimulation.addEventListener("click", (event) => {
-        window.confirm("Confirm that you want to run the simulation.")
+//     runSimulation.addEventListener("click", (event) => {
+//         let form = document.querySelector("form");
+//         form.addEventListener("submit", (event) => {
+            
+//         })
+//     });
+
+// }
+
+// window.addEventListener("load", init);
+
+window.addEventListener("load", function () {
+    let form = document.querySelector("form");
+    form.addEventListener("submit", (event) => {
+        let testNameInput = document.querySelector("input[name=testName]");
+        let testDateInput = document.querySelector("input[name=testDate]");
+        if (
+            testNameInput.value === ""
+            && testDateInput.value === ""
+            
+            ) {
+            alert("All fields are required!");
+            event.preventDefault();
+        }
     });
-
-}
+});
